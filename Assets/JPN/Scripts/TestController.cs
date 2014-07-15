cb3f370222ba0315bae1aa94fd53c4707b759e3c
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-07-15 23:42:20 +0900

    画面スクロール設置

---
+++ Assets/JPN/Scripts/TestController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestController : MonoBehaviour {
+
+	public GameObject stagePrefab;
+	public GameObject scrollView;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0;i <5;i++){
+			GameObject stageObject = Instantiate(stagePrefab)as GameObject;
+			stageObject.transform.parent = scrollView.transform;
+			stageObject.transform.localScale = new Vector3(1,1,1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
