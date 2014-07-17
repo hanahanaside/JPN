739255d27dc8b6bcab768af1b9e64e224666e4d1
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-07-17 21:47:26 +0900

    作業開始

--- Assets/JPN/Scripts/TestController.cs
+++ Assets/JPN/Scripts/TestController.cs
@@ -5,13 +5,16 @@ public class TestController : MonoBehaviour {
 
 	public GameObject stagePrefab;
 	public GameObject scrollView;
+	public UIGrid uiGrid;
 
 	// Use this for initialization
 	void Start () {
-		for(int i = 0;i <5;i++){
+		for(int i = 0;i <4;i++){
 			GameObject stageObject = Instantiate(stagePrefab)as GameObject;
-			stageObject.transform.parent = scrollView.transform;
+			uiGrid.AddChild(stageObject.transform);
 			stageObject.transform.localScale = new Vector3(1,1,1);
+			UISprite sprite = stageObject.GetComponentInChildren<UISprite>();
+			sprite.spriteName = "bg_main_"+i;
 		}
 	}
 	
