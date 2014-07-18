571e75683d01092f48349b3bc48e3d87cb87387e
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-07-18 12:41:13 +0900

    ステージのGUIから

--- Assets/JPN/Scripts/Main/MainController.cs
+++ Assets/JPN/Scripts/Main/MainController.cs
@@ -3,21 +3,5 @@ using System.Collections;
 
 public class MainController : MonoBehaviour {
 
-	public GameObject prefab;
-	public UIGrid grid;
 
-	// Use this for initialization
-	void Start () {
-		for(int i = 0; i<9;i++) {
-			GameObject stageObject = Instantiate (prefab) as GameObject;
-			grid.AddChild (stageObject.transform);
-			stageObject.transform.localScale = new Vector3 (1,1,1);
-
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ Assets/JPN/Scripts/Main/MainInitializer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainInitializer : MonoBehaviour {
+
+	public GameObject homePrefab;
+	public GameObject stagePrefab; 
+	public UIGrid grid;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject homeObject = Instantiate (homePrefab) as GameObject;
+		AddChild (homeObject);
+		for (int i = 0; i<9; i++) {
+			GameObject stageObject = Instantiate (stagePrefab) as GameObject;
+			AddChild (stageObject);
+		}
+	}
+
+	private void AddChild (GameObject panelObject) {
+		grid.AddChild (panelObject.transform);
+		panelObject.transform.localScale = new Vector3 (1, 1, 1);
+	}
+
+}
