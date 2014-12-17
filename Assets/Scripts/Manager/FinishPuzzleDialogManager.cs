f463f7bb6158a330a9972bfe73365f25b07e3b9b
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-12-17 10:08:36 +0900

    作業を開始

---
+++ Assets/Scripts/Manager/FinishPuzzleDialogManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FinishPuzzleDialogManager : MonoBehaviour {
+
+	public static event Action BackToStageEvent;
+	public static event Action<int> RetryEvent;
+
+	public void OnBackToStageClicked(){
+		BackToStageEvent();
+	}
+
+	public void OnRetryClicked(){
+		RetryEvent (0);
+	}
+
+	public void Show(){
+
+	}
+}
