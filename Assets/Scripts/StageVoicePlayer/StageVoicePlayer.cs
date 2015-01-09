074b427cc8dbe9bb5452bd56f8d571c30f10c5bb
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2015-01-09 12:24:15 +0900

    ボイス管理を実装

---
+++ Assets/Scripts/StageVoicePlayer/StageVoicePlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageVoicePlayer : MonoSingleton<StageVoicePlayer> {
+
+	public UIGrid stageGrid;
+	private int mVoiceIndex;
+
+	public override void OnInitialize () {
+		UICenterOnChild uiCenterOnChild = stageGrid.GetComponent<UICenterOnChild> ();
+		uiCenterOnChild.onCenter += OnCenterCallBack;
+	}
+
+	void OnCenterCallBack (GameObject centeredObject) {
+		CancelInvoke ();
+		int index = stageGrid.GetIndex (centeredObject.transform);
+		if (index >= 2) {
+			mVoiceIndex = index - 2;
+			float rand = UnityEngine.Random.Range (0f, 3.0f);
+			Invoke ("PlayVoice", rand);
+		}
+	}
+
+	private void PlayVoice () {
+		CharacterVoiceManager.instance.PlayVoice (mVoiceIndex);
+	}
+}
