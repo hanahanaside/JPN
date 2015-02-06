bb3c95455a68035e967f857d472ec216928ae9bc
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2015-02-06 17:14:35 +0900

    最新版

--- Assets/Scripts/Test/TestController.cs
+++ Assets/Scripts/Test/TestController.cs
@@ -1,19 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 public class TestController : TestParent {
 
 	public TestController2 hoge;
+	public UILabel label;
 
 	void Start () {
-
+	//	label.text = "111111111111111111";
 	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.Escape)) {
-			Application.Quit ();
-		}
+
 
 	}
 
